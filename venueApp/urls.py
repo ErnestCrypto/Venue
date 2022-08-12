57f3e7ad8d827d364f5b75d9a6c185b22843d643
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-08-12 09:24:39 +0000

    commit

--- venueApp/urls.py
+++ venueApp/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('venue/', views.VenueList.as_view(), name='venuelist'),
     path('artist/', views.ArtistList.as_view(), name='Artistlist'),
     path('shows/', views.ShowsList.as_view(), name='showslist'),
-    path('venuedetail/', views.VenueDetails.as_view(), name='venuedetails'),
-    path('artistdetail/', views.ArtistDetails.as_view(), name='artistdetails'),
-    path('showdetail/', views.ShowsDetails.as_view(), name='showsdetails'),
+    path('venuedetail/<str:pk>/', views.VenueDetails.as_view(), name='venuedetails'),
+    path('artistdetail/<str:pk>/',
+         views.ArtistDetails.as_view(), name='artistdetails'),
+    path('showdetail/<str:pk>/', views.ShowsDetails.as_view(), name='showsdetails'),
 
 
 
