14e23c570dbcf78b4225eb3397cd178f49d58603
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-08-12 08:39:18 +0000

    commit

--- venueApp/serializers.py
+++ venueApp/serializers.py
@@ -5,13 +5,16 @@ from .models import Venue, Artist, Shows
 class VenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venue
+        fields = '__all__'
 
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
+        fields = '__all__'
 
 
 class ShowsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shows
+        fields = '__all__'
